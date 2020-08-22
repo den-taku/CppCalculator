800e80f993a30e6a5b211f5e0b962af0e342c753
Tada <sevensins0410s@icloud.com>
2020-08-23 04:28:06 +0900

    define RPN and Operator

--- src/cpp_cal.cpp
+++ src/cpp_cal.cpp
@@ -1,27 +1,77 @@
 #include <iostream>
 #include <cstring>
+#include <string>
 using namespace std;
 
 class Calculator {
     public:
-        Calculator(char input[]);
+        Calculator(const char input[]);
         void print_for_test();
     private:
-        char input_str[256];
+        char str[256];
+        char notated_str[256];
 };
 
-Calculator::Calculator(char input[]) {
-    strcpy(input_str, input);
+class RPN {
+    public:
+        RPN(const char input[]);
+        void notate(char output[]);
+    private:
+        char str[256];
+};
+
+class Operator {
+    public:
+        Operator(const char input[]);
+        void calculate();
+    private:
+        char str[256];
+};
+
+Calculator::Calculator(const char input[]) {
+    // initialize Calculator
+    strcpy(str, input);
+    print_for_test();
+    cout << " = " << flush;
+
+    // renotate input as Reverse Polish Notation
+    RPN rpn(str);
+    rpn.notate(notated_str);
+
+    // calculate notated_str and print result
+    Operator oper(notated_str);
+    oper.calculate();
 }
 
-void Calculator::print() {
-    cout << input_str << endl;
+void Calculator::print_for_test() {
+    cout << str << flush;
+}
+
+RPN::RPN(const char input[]) {
+    strcpy(str, input);
+}
+void RPN::notate(char output[]){
+    char buffer[256];
+    // str -> RPN -> buffer
+    strcpy(output, buffer);
+}
+
+Operator::Operator(const char input[]) {
+    strcpy(str, input);
+}
+
+void Operator::calculate() {
+    float answer = 0;
+    // calulate as RPN -> answer
+    cout << answer << endl;
 }
 
 int main() {
-    Calculator cal("hoge\n");
-    cal.print_for_test();
-    cout << "Hello, world!" << endl;
+    cout << "Hello! Let's calculate!!" << endl;
+    cout << "Input expression like 1+(2*3)/4*5" << endl;
+    string input;
+    getline(cin, input);
+    Calculator cal(input.c_str());
 }
 
 
